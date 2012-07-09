689f5793bf375422bf8da91916dec2744ba82006
soudaou <souaddaou@gmail.com>
2012-07-09 15:25:56 -0400

    shifted things around

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] =='POST') {
 		exit;
 	}
 		$user_id = $user['id'];	
-		
 		if ($user_id) {
 			session_regenerate_id();
 			$_SESSION['user_id'] = $user_id;
@@ -53,40 +52,38 @@ if ($_SERVER['REQUEST_METHOD'] =='POST') {
 <body>
 	<header>
 		<div class="logo">
-			<a href="index.html"><img src="images/logo1.png" alt="Just Design logo"></a>
+			<a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a>
 		</div>
 		<nav>
 			<ul>
-				<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
-				<li><a href="#"><strong> My List </strong></a></li>
-				<li><a href="#"><strong> Edit List </strong></a></li>
+				<li><a href="create-list.php"> <strong> Create my List </strong> </a></li>
+				<li><a href="my-list.php"><strong> My List </strong></a></li>
+				<li><a href="edit-list.php"><strong> Edit List </strong></a></li>
 			</ul>
 		</nav>
 	</header>
 
 	<div class="content">
 	
-		<div class="slogan">
-			<p> <strong> This application is for exercise fanatics </strong> </p>
-		</div>
-		
-		<form method="post" action="index.php">
-			 <div>
-			 <h1>Sign in:</h1>
-				<label for="email">
-					<strong> Email </strong>
-					<?php if (isset($errors['email'])): ?> 
-					<strong class="error">is required</strong> 
-					<?php endif; ?>
-				</label>
-				<input id="email" name="email" required value="<?php echo $email; ?>">
+		<h1> <strong> This application is for exercise fanatics </strong> </h1>
+		<div class="content">
+			<div class="container">
+				<form method="post" action="index.php">
+					 <div>
+					 <h1>Sign in:</h1>
+						<label for="email">
+							<strong> Email </strong>
+							<?php if (isset($errors['email'])): ?> 
+							<strong class="error">is required</strong> 
+							<?php endif; ?>
+						</label>
+						<input id="email" name="email" required value="<?php echo $email; ?>">
+					</div>
+			
+					<button types="submit">Sign In</button>
+				</form>
+				<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a>
 			</div>
-	
-			<button types="submit">Sign In</button>
-		</form>
-		
-		<div class="sign-up">
-			<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a>
 		</div>
 		
 	</div>
