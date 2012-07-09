dda39d417ecf848c9efe9472cf70e3c190256a02
soudaou <souaddaou@gmail.com>
2012-07-09 14:43:10 -0400

    had a misspelling

--- sign-up.php
+++ sign-up.php
@@ -1,6 +1,5 @@
 <?php 
 
-require_once 'includes/form-processor.php';
 
 $errors = array();
 
@@ -35,23 +34,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	<title>Registration Form Validation</title>
 	<link href="css/general.css" rel="stylesheet">
 </head>
+	<header>
+		<h1><a href="index.html"><img src="images/logo1.png" alt="Just Design logo"></a></h1>
+		<nav>
+			<ul>
+				<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
+				<li><a href="#"><strong> My List </strong></a></li>
+				<li><a href="#"><strong> Edit List </strong></a></li>
+			</ul>
+		</nav>
+	</header>
 
 <body>
-
-	<h1>Sign up:</h1>
-    
-	<form method="post" action="sign-up.php">
-         <div>
-            <label for="email">
-            	<strong> Email </strong>
-				<?php if (isset($errors['email'])): ?> 
-            	<strong class="error">is required</strong> 
-				<?php endif; ?>
-            </label>
-            <input id="email" name="email" required value="<?php echo $email; ?>">
-        </div>
-
-        <button types="submit">Sign Up</button>
-    </form>
+	
+	<div class="container">
+		<h1>Sign up:</h1>
+		<form method="post" action="sign-up.php">
+			 <div>
+				<label for="email">
+					<strong> Email </strong>
+					<?php if (isset($errors['email'])): ?> 
+					<strong class="error">is required</strong> 
+					<?php endif; ?>
+				</label>
+				<input id="email" name="email" required value="<?php echo $email; ?>">
+			</div>
+	
+			<button types="submit">Sign Up</button>
+		</form>
+	</div>
+	
 </body>
 </html>
