2a482a6a6ae1a215db9184ffde46bc1f55dba637
soudaou <souaddaou@gmail.com>
2012-07-09 13:04:15 -0400

    added the php code for the sign-up

--- sign-up.php
+++ sign-up.php
@@ -2,6 +2,32 @@
 
 require_once 'includes/form-processor.php';
 
+$errors = array();
+
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_NUMBER_INT);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	if (strlen($email) < 1 || strlen($email) > 256) {
+		$errors['email'] = true;
+	}
+
+	if (empty($errors)) {
+		
+		require_once 'includes/db.php';
+		
+		$sql = $db->prepare('
+			INSERT INTO mep-users (email)
+			VALUES (:email)
+		');
+		$sql->bindValue(':email', $email, PDO::PARAM_INT);
+		$sql->execute();
+		
+		header('Location: sign-in.php');
+		exit;
+	}
+}
+
 ?><!DOCTYPE HTML>
 <html>
 <head>
@@ -17,7 +43,7 @@ require_once 'includes/form-processor.php';
 	<form method="post" action="index.php">
          <div>
             <label for="email">
-            	<strong>Email </strong>
+            	<strong> Email </strong>
 				<?php if (isset($errors['email'])): ?> 
             	<strong class="error">is required</strong> 
 				<?php endif; ?>
