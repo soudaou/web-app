82f3269d6a525e4860bec1716738ce6dcf306eef
soudaou <souaddaou@gmail.com>
2012-07-09 14:07:50 -0400

    fixed the code added a div class="content" to css

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php
-
 require_once 'includes/db.php';
 
-$sql = $db->query('
-	SELECT id, email
-	FROM mep_users
-');
+$errors = array();
 
-//var_dump($db->errorInfo());
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 
-$results = $sql->fetchALL();
+if ($_SERVER['REQUEST_METHOD'] =='POST') {
+	if (empty($email)) {
+		$errors['email'] = true;
+	}
+	
+	if (empty($errors)) {
+		$sql = $db->prepare('
+		SELECT id, email
+		FROM mep_users
+		WHERE email = :email
+		LIMIT 1
+	');
+	$sql->bindValue(':email', $email, PDO::PARAM_STR);
+	$sql->execute();
+	$user = $sql->fetch();
+	
+	if (empty($user)) {
+		header('Location: sign-up.php');
+		exit;
+	}
+		$user_id = $user['id'];	
+		
+		if ($user_id) {
+			session_regenerate_id();
+			$_SESSION['user_id'] = $user_id;
+			header('Location: main-pg.php' );
+			exit;
+			//redirect back to the page they came from
+		} 
+		else {
+			$errors['no-user'] = true;
+		}
+	}
+}
 
 ?><!DOCTYPE HTML>
 <html>
@@ -23,23 +52,41 @@ $results = $sql->fetchALL();
 
 <body>
 	<header>
-<!--		<h1><a href="index.html"><img src="images/logo.png" alt="Just Design logo"></a></h1>
--->		<nav>
+		<h1><a href="index.html"><img src="images/logo1.png" alt="Just Design logo"></a></h1>
+		<nav>
 			<ul>
-				<li><a href="create-list.html">Create my List</a></li>
-				<li><a href="#">My List</a></li>
-				<li><a href="#">Edit List</a></li>
+				<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
+				<li><a href="#"><strong> My List </strong></a></li>
+				<li><a href="#"><strong> Edit List </strong></a></li>
 			</ul>
 		</nav>
 	</header>
+
+	<div class="content">
 	
+		<div class="slogan">
+			<p> <strong> This application is for exercise fanatics </strong> </p>
+		</div>
+		
+		<form method="post" action="index.php">
+			 <div>
+			 <h1>Sign in:</h1>
+				<label for="email">
+					<strong> Email </strong>
+					<?php if (isset($errors['email'])): ?> 
+					<strong class="error">is required</strong> 
+					<?php endif; ?>
+				</label>
+				<input id="email" name="email" required value="<?php echo $email; ?>">
+			</div>
 	
-	<div class="slogan">
-		<p> <strong> This application is for exercise fanatics </strong> </p>
-	</div>
-	
-	<div class="sign-up">
-		<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a>
+			<button types="submit">Sign In</button>
+		</form>
+		
+		<div class="sign-up">
+			<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a>
+		</div>
+		
 	</div>
 
 	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
