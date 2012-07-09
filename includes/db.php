e5dffa9718ccbdf11cca7d1e7ca721241b9a84d8
soudaou <souaddaou@gmail.com>
2012-07-09 14:08:10 -0400

    made changes that i shouldnt have. fixed it

--- includes/db.php
+++ includes/db.php
@@ -7,13 +7,12 @@
 // This is for security
 $user = getenv('DB_USER');		// The MySQL username
 $pass = getenv('DB_PASS');		// The MySQL password
-$email = getenv('DB_EMAIL');	// The MySQL email
 //no spaces here at all!!
 $data_source = getenv('DATA_SOURCE');
 
 // PDO: PHP Data Objects
 // Allows us to connect to many different kinds of database
-$db = new PDO($data_source, $user, $pass, $email);
+$db = new PDO($data_source, $user, $pass);
 
 // Force the connection to commmunicate in UTF-8
 // and support many human languages
