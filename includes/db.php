7852071c7aac78e798dba29a72e7d5ee2b0f9835
soudaou <souaddaou@gmail.com>
2012-07-09 13:06:17 -0400

    added a variable for the email

--- includes/db.php
+++ includes/db.php
@@ -5,14 +5,15 @@
 
 // Our username and password are kept in Environment Variables, in .htaccess
 // This is for security
-$user = getenv('DB_USER');	// The MySQL username
-$pass = getenv('DB_PASS');	// The MySQL password
+$user = getenv('DB_USER');		// The MySQL username
+$pass = getenv('DB_PASS');		// The MySQL password
+$email = getenv('DB_EMAIL');	// The MySQL email
 //no spaces here at all!!
 $data_source = getenv('DATA_SOURCE');
 
 // PDO: PHP Data Objects
 // Allows us to connect to many different kinds of database
-$db = new PDO($data_source, $user, $pass);
+$db = new PDO($data_source, $user, $pass, $email);
 
 // Force the connection to commmunicate in UTF-8
 // and support many human languages
