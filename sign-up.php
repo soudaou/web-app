6639ba50aaf8c35c53225f35eb39b040b2fd2fe1
soudaou <souaddaou@gmail.com>
2012-07-16 10:01:58 -0400

    added extra div to body to give proper css

--- sign-up.php
+++ sign-up.php
@@ -34,37 +34,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	<title>Registration Form Validation</title>
 	<link href="css/general.css" rel="stylesheet">
 </head>
-	<header>
-		<h1><a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a></h1>
-		<nav>
-			<ul>
-				<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
-				<li><a href="#"><strong> My List </strong></a></li>
-				<li><a href="#"><strong> Edit List </strong></a></li>
-			</ul>
-		</nav>
-	</header>
 
 <body>
+	<div id="body-wrapper">
+		<header>
+			<h1><a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a></h1>
+			<nav>
+				<ul>
+					<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
+					<li><a href="#"><strong> My List </strong></a></li>
+					<li><a href="#"><strong> Edit List </strong></a></li>
+				</ul>
+			</nav>
+		</header>
 	
-	<div class="content">
-		<div class="container">
-			<h1>Sign up:</h1>
-			<form method="post" action="sign-up.php">
-				 <div>
-					<label for="email">
-						<strong> Email </strong>
-						<?php if (isset($errors['email'])): ?> 
-						<strong class="error">is required</strong> 
-						<?php endif; ?>
-					</label>
-					<input id="email" name="email" required value="<?php echo $email; ?>">
+		<div class="content">
+			<div class="container">
+				<div class="login-info">
+					<div class="top-login">
+						<img src="images/lock.jpg" alt="">
+							<h2>Sign up:</h2>	
+					</div>
+					<div class="form-login">
+						<form method="post" action="sign-up.php">
+							 <div>
+								<label for="email">
+									<strong> Email </strong>
+									<?php if (isset($errors['email'])): ?> 
+									<strong class="error">is required</strong> 
+									<?php endif; ?>
+								</label>
+								<input id="email" name="email" required value="<?php echo $email; ?>">
+							</div>
+							<button types="submit"><strong>Sign up</strong></button>
+						</form>
+					</div>
 				</div>
-		
-				<button types="submit">Sign Up</button>
-			</form>
+			</div>
 		</div>
 	</div>
-	
 </body>
 </html>
