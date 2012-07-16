76ac38b9d61e3efcfe09d719cfed2fb6358fca00
soudaou <souaddaou@gmail.com>
2012-07-16 10:01:33 -0400

    added some div to body to give proper css

--- index.php
+++ index.php
@@ -50,45 +50,50 @@ if ($_SERVER['REQUEST_METHOD'] =='POST') {
 </head>
 
 <body>
-	<header>
-		<div class="logo">
-			<a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a>
-		</div>
-		<nav>
-			<ul>
-				<li><a href="create-list.php"> <strong> Create my List </strong> </a></li>
-				<li><a href="my-list.php"><strong> My List </strong></a></li>
-				<li><a href="edit-list.php"><strong> Edit List </strong></a></li>
-			</ul>
-		</nav>
-	</header>
+	<div id="body-wrapper">
+		<header>
+			<div class="logo">
+				<a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="create-list.php"> <strong> Create my List </strong> </a></li>
+					<li><a href="my-list.php"><strong> My List </strong></a></li>
+					<li><a href="edit-list.php"><strong> Edit List </strong></a></li>
+				</ul>
+			</nav>
+		</header>
 
-	<div class="content">
-	
-		<h1> <strong> This application is for exercise fanatics </strong> </h1>
 		<div class="content">
+			<h1> <strong> This application is for exercise fanatics </strong> </h1>
 			<div class="container">
-				<form method="post" action="index.php">
-					 <div>
-					 <h1>Sign in:</h1>
-						<label for="email">
-							<strong> Email </strong>
-							<?php if (isset($errors['email'])): ?> 
-							<strong class="error">is required</strong> 
-							<?php endif; ?>
-						</label>
-						<input id="email" name="email" required value="<?php echo $email; ?>">
+				<div class="login-info">
+					<div class="top-login">
+						<img src="images/lock.jpg" alt="">
+						<h2>Sign in:</h2>	
 					</div>
-			
-					<button types="submit">Sign In</button>
-				</form>
-				<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a>
+					<div class="form-login">
+						<form method="post" action="index.php">
+								<div>
+								<label for="email">
+									<strong> Email </strong>
+									<?php if (isset($errors['email'])): ?> 
+									<strong class="error">is required</strong> 
+									<?php endif; ?>
+								</label>
+								<input id="email" name="email" required value="<?php echo $email; ?>">
+							</div>
+							<p>If you are a new user, <a href="sign-up.php"><strong>click here</strong></a> to register</p>
+							<button types="submit"><strong>Sign in</strong></button>
+						</form>
+		<!--			<a href="sign-up.php"><img src="images/sign-up.png" alt=""></a> -->
+					</div>
+				</div>
 			</div>
 		</div>
-		
-	</div>
 
 	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
 	<!--<script src="js/my-list.js"></script>-->
+	</div>
 </body>
 </html>
