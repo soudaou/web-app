c9f75177e060a2cce8a7f0da099a1166fee33a7f
soudaou <souaddaou@gmail.com>
2012-07-16 15:17:28 -0400

    fixing code

--- edit-list.php
+++ edit-list.php
@@ -1,56 +1,108 @@
 <?php
 
 require_once 'includes/db.php';
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$errors = array();
 
-// ->prepare() allows us to have variables in our sql
-// we can create placeholders and later fill them with some content
-// By using prepare we are protecting against SQL injections attact
-// PLaceholders are marked with a colon (:) in front of them
-$sql = $db->prepare('
-	SELECT id, email
-	FROM mep_users
-	WHERE id =  :id
+$user_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$name_exercise = filter_input(INPUT_POST, 'name_exercise', FILTER_SANITIZE_STRING);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	if (!isset($_POST['terms'])){
+		$errors['terms'] = true;
+	}
+	if (empty($errors)) {
+
+		$sql = $db->prepare('
+			UPDATE mep_exerciselist
+			SET name_exercise = :name_exercise
+			WHERE user_id = :user_id
+		');
+		$sql->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+		$sql->bindValue(':name_exercise', $name_exercise, PDO::PARAM_STR);
+		$sql->execute();
+		
+		header('Location: my-list.php');
+		exit;
+	}
+}
+
+
+else{
+	$sql = $db->prepare('
+		SELECT l.user_id, l.exercise_id, e.name_exercise
+		FROM mep_exerciselist as l
+		INNER JOIN mep_exercises as e
+		ON l.exercise_id = e.id
+		WHERE l.user_id = :user_id
 ');
-// bindValue(placeholder, variable, datatype)
-$sql->bindValue(':id', $id, PDO::PARAM_INT);
-$sql->execute();
-// if you are going to fetch one thing only you type only fetch(); rather than fetchALL();
-$results = $sql->fetch();
+	$sql->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+	$sql->execute();
+	$results = $sql->fetch();
+	
+	//print_r($results);
+	
+	$user_id = $results['user_id'];
+	$name_exercise = $results['name_exercise'];
+}
+
 
 ?><!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Edit List</title>
+<meta charset="utf-8">
+	<title>My XERCISE Planner</title>
 	<meta name="viewport" content="width=device-width">
 	<link href="css/general.css" rel="stylesheet">
+	
 </head>
 
 <body>
-	<header>
-		<div class="logo">
-			<a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a>
+	<div id="body-wrapper">
+		<header>
+			<div class="logo">
+				<a href="index.php"><img src="images/logo1.png" alt="Just Design logo"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="create-list.php"> <strong> Create my List </strong> </a></li>
+					<li><a href="my-list.php"><strong> My List </strong></a></li>
+					<li><a href="edit-list.php"><strong> Edit List </strong></a></li>
+					<li><a href="log-out.php"><strong>Log out</strong></a></li>
+				</ul>
+			</nav>
+		</header>
+	
+		<div class="content">
+			<div class="exercise-list">
+					
+			</div>
+				<form method="post" actions="edit.php?id=<?php echo $name_exercise; ?> ">
+					<fieldset>
+						<legend>
+							<h2><strong>Edit List: </strong></h2>
+						</legend>
+						<div>
+						
+						<label for="check">
+						<?php foreach ($results as $name_exercise) : ?>
+						<input type="checkbox" id="check" name="check" value="<?php echo $name_exercise; ?>">
+							<p><?php echo $exercise['name_exercise']; ?></p>
+						<?php endforeach ?>
+			<!--            	Check 7 Exercises--> 
+						<?php if (isset($errors['terms'])): ?> 
+							<strong class="error">Must choose 7 exercises!</strong> 
+						<?php endif; ?>
+						</label>
+						</div>
+					</fieldset>
+					<button type="submit">Update</button>
+				
+				</form>
 		</div>
-		<nav>
-			<ul>
-				<li><a href="create-list.html"> <strong> Create my List </strong> </a></li>
-				<li><a href="#"><strong> My List </strong></a></li>
-				<li><a href="#"><strong> Edit List </strong></a></li>
-			</ul>
-		</nav>
-	</header>
 	
-	
-	<h1> <?php echo $results['dino_name']; ?></h1>
-	<dl>
-		<dt> Loves Meat </dt>
-		<dd><?php echo $results['loves_meat']; ?></dd>
-
-	</dl>
-	
-	<a href="edit.php?id=<?php echo $id; ?>"> Edit </a>
-	<a href="delete.php?id=<?php echo $id; ?>"> Delete </a>
-	
+		<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
+		<!--<script src="js/my-list.js"></script>-->
+	</div>
 </body>
 </html>
