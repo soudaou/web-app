d9656d2fc538722d0c486deff82f203c9674390b
soudaou <souaddaou@gmail.com>
2012-07-20 09:34:11 -0400

    fixed code for checking the users exercise id

--- edit-list.php
+++ edit-list.php
@@ -1,50 +1,39 @@
 <?php
-
+session_start();
 require_once 'includes/db.php';
 $errors = array();
 
-$user_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$name_exercise = filter_input(INPUT_POST, 'name_exercise', FILTER_SANITIZE_STRING);
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-	if (!isset($_POST['terms'])){
-		$errors['terms'] = true;
-	}
-	if (empty($errors)) {
-
 		$sql = $db->prepare('
-			UPDATE mep_exerciselist
-			SET name_exercise = :name_exercise
-			WHERE user_id = :user_id
+		SELECT  id, name_exercise
+		FROM mep_exercises
 		');
-		$sql->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-		$sql->bindValue(':name_exercise', $name_exercise, PDO::PARAM_STR);
-		$sql->execute();
+
+//var_dump($db->errorInfo());
+$sql->execute();
+$results = $sql->fetchALL();
+
+//print_r($results);
 		
-		header('Location: my-list.php');
-		exit;
-	}
-}
-
-
-else{
-	$sql = $db->prepare('
+		
+		$sql = $db->prepare('
 		SELECT l.user_id, l.exercise_id, e.name_exercise
 		FROM mep_exerciselist as l
 		INNER JOIN mep_exercises as e
 		ON l.exercise_id = e.id
 		WHERE l.user_id = :user_id
-');
-	$sql->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-	$sql->execute();
-	$results = $sql->fetch();
-	
-	//print_r($results);
-	
-	$user_id = $results['user_id'];
-	$name_exercise = $results['name_exercise'];
+		');
+		$sql->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_STR);
+$sql->execute();
+$userex = $sql->fetchALL();
+
+$user_exercise_ids = array();
+
+foreach ($userex as $ue) {
+	$user_exercise_ids[] = $ue['exercise_id'];
 }
+
+//print_r($userex);
+
 
 
 ?><!DOCTYPE HTML>
@@ -75,29 +64,36 @@ else{
 	
 		<div class="content">
 			<div class="exercise-list">
-					
-			</div>
-				<form method="post" actions="edit.php?id=<?php echo $name_exercise; ?> ">
-					<fieldset>
-						<legend>
-							<h2><strong>Edit List: </strong></h2>
-						</legend>
-						<div>
-						
-						<label for="check">
-						<?php foreach ($results as $name_exercise) : ?>
-						<input type="checkbox" id="check" name="check" value="<?php echo $name_exercise; ?>">
-							<p><?php echo $exercise['name_exercise']; ?></p>
-						<?php endforeach ?>
-			<!--            	Check 7 Exercises--> 
-						<?php if (isset($errors['terms'])): ?> 
-							<strong class="error">Must choose 7 exercises!</strong> 
+				</div>
+				<form method="post" actions="edit-list.php">
+				<fieldset>
+					<legend>
+						<h2><strong>Choose 7 Exercise for your week: </strong></h2>
+						<?php if (isset($errors['exerciseCheck'])): ?> 
+						<strong class="error">Must choose 7!</strong> 
 						<?php endif; ?>
+					</legend>
+					
+					<div class="check-list">
+						<label for="check">
+								<?php foreach ($results as $exercise) : ?>
+									<div class="check-box">
+										<input type="checkbox" id="checkbox" name="checkbox" value="1" <?php if (in_array($exercise['id'], $user_exercise_ids)) : ?>checked<?php endif; ?>>
+									</div>
+									<div class="exercise-list">
+										<ul>
+											<li><strong><?php echo $exercise['name_exercise']; ?></strong></li>
+										</ul>
+									</div>
+								<?php endforeach ?>
+							
+							<?php if (isset($errors['terms'])): ?> 
+								<strong class="error">Must choose 7 exercises!</strong> 
+							<?php endif; ?>
 						</label>
-						</div>
-					</fieldset>
-					<button type="submit">Update</button>
-				
+					</div>
+				</fieldset>
+					<button type="submit">Save</button>
 				</form>
 		</div>
 	
