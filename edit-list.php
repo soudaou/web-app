d5a16009d3029a9ac11fbdbf0fef56e29f7745a2
soudaou <souaddaou@gmail.com>
2012-07-20 12:17:29 -0400

    added code that was wrong, had to fix it and resave it again

--- edit-list.php
+++ edit-list.php
@@ -3,6 +3,29 @@ session_start();
 require_once 'includes/db.php';
 $errors = array();
 
+$checkbox = filter_input(INPUT_POST, 'checkbox', FILTER_SANITIZE_NUMBER_INT, FILTER_FORCE_ARRAY);
+if (!is_array($checkbox)) $checkbox = array();
+/*Validation*/
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+		
+		if (count($checkbox) != 7){
+			$errors['checkbox'] = true;
+		}
+		if (empty($errors)) {
+			$sql = $db->prepare('
+			INSERT INTO mep_exerciselist (name_exercise, id )
+			VALUES (:name_exercise, :id :)
+			');
+			$sql->bindValue(':name_exercise', $name_exercise, PDO::PARAM_STR);
+			$sql->bindValue(':id', $id, PDO::PARAM_INT);
+			$sql->execute();
+			var_dump($db->errorInfo());
+			header('Location: my-list.php');
+			exit;
+		}
+}
+
 		$sql = $db->prepare('
 		SELECT  id, name_exercise
 		FROM mep_exercises
@@ -13,8 +36,7 @@ $sql->execute();
 $results = $sql->fetchALL();
 
 //print_r($results);
-		
-		
+
 		$sql = $db->prepare('
 		SELECT l.user_id, l.exercise_id, e.name_exercise
 		FROM mep_exerciselist as l
@@ -27,13 +49,10 @@ $sql->execute();
 $userex = $sql->fetchALL();
 
 $user_exercise_ids = array();
-
 foreach ($userex as $ue) {
-	$user_exercise_ids[] = $ue['exercise_id'];
+$user_exercise_ids[] = $ue['exercise_id'];
 }
-
 //print_r($userex);
-
 
 
 ?><!DOCTYPE HTML>
@@ -67,16 +86,12 @@ foreach ($userex as $ue) {
 				</div>
 				<form method="post" actions="edit-list.php">
 				<fieldset>
-					<legend>
-						<h2><strong>Choose 7 Exercise for your week: </strong></h2>
-						<?php if (isset($errors['exerciseCheck'])): ?> 
-						<strong class="error">Must choose 7!</strong> 
-						<?php endif; ?>
-					</legend>
-					
+					<legend><h2><strong>Choose 7 Exercise for your week: </strong></h2></legend>
+					<?php if (isset($errors['checkbox'])): ?> <strong class="error">You must choose 7!</strong> <?php endif; ?>
 					<div class="check-list">
 						<label for="check">
 								<?php foreach ($results as $exercise) : ?>
+								
 									<div class="check-box">
 										<input type="checkbox" id="checkbox" name="checkbox" value="1" <?php if (in_array($exercise['id'], $user_exercise_ids)) : ?>checked<?php endif; ?>>
 									</div>
@@ -86,10 +101,7 @@ foreach ($userex as $ue) {
 										</ul>
 									</div>
 								<?php endforeach ?>
-							
-							<?php if (isset($errors['terms'])): ?> 
-								<strong class="error">Must choose 7 exercises!</strong> 
-							<?php endif; ?>
+
 						</label>
 					</div>
 				</fieldset>
