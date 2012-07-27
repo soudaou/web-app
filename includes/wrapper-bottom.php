d2d9f602adc4c7377cf52c112e094f8903a5edfa
soudaou <souaddaou@gmail.com>
2012-07-27 12:35:34 -0400

    created a wrapper-bottom

---
+++ includes/wrapper-bottom.php
@@ -0,0 +1,10 @@
+</div>
+	<div class="copyright">
+		<footer>
+			<p>my xercise app © 2012 </p>
+		</footer>
+		<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
+		<!--<script src="js/my-list.js"></script>-->
+	</div>
+</body>
+</html>
